18910eb5db6934fc262b43f660e56cf5a9db5ed9
Kodadam45 <bunyaminefsanesi@gmail.com>
2024-07-23 16:14:37 +0300

    Create Toolu-öğrenme.py

---
+++ Toolu-öğrenme.py
@@ -0,0 +1,9 @@
+print("tool türkçedir")
+print("tool a hoş geldiniz")
+print("bu tool download klasörüne kısa yoldan gitmek için var")
+
+
+print("cd yazın")
+print("cd kodadqm yazın")
+print("bash Download.sh yazın")
+print("işlemi tamamladınız")
